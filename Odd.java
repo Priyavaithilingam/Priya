ab2aa4e2dd3d1271b712ac2edc8260870348a81a
Priya.V <priyasenthila@gmail.com>
2016-09-02 12:13:04 +0530

    Add files via upload

---
+++ Odd.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class Odd
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int i,sum=0;
+for(i=1;i<=n;i=i+2)
+{
+  sum+=i;
+}
+System.out.println(sum);
+}
+}
