4a39b7847eb3162a45dd54a5a127cb182c2d9520
Priya.V <priyasenthila@gmail.com>
2017-09-07 22:25:05 +0530

    Add files via upload

---
+++ Reverse1.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+class Reverse1
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    StringBuilder sb1=new StringBuilder();
+    int i;
+    String s1=sc.nextLine();
+    String[] s2=s1.split(" ");
+    for(i=s2.length;i>0;i--)
+    {
+      sb1.append(s2[i-1]);
+      sb1.append(" ");
+    }
+    System.out.println(sb1);
+    
+  }
+}
+    
+    
